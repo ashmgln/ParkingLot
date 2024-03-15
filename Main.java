85406f780f5818bbe24d527d85b34343c8bb89aa
Ashish Miglani <ashishmiglani02@gmail.com>
2024-03-16 02:20:00 +0530

    added main

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+
+/*
+
+Vehicle Type -> 4w, 2w, Large
+Multifloor -> Yes
+Allocation -> Yes
+Pricing -> No
+Space Utilising by 4w in large Space -> No
+Space Utilising by 2w in 4w Space -> Yes
+Parking Strategy -> Ground to Upper floor, nearest first
+
+ */
+
+/*
+Vehicle Interface
+
+//Class
+4w
+2w
+large
+
+AllocationStrategy Interface
+
+ParkingLot Class
+Floor Class
+Parking Slot Interface
+2w slot class
+4w slot class
+large slot class
+
+ParkingController
+
+
+*/
